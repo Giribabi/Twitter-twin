a2d8f5554ac8adcc876c48058ccd6b9cf2004ead
Giribabi <sammidisaroja@gmail.com>
2024-06-02 19:42:36 +0530

    Designed the widgets section

--- frontend/src/Components/Widgets/Widgets.jsx
+++ frontend/src/Components/Widgets/Widgets.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import "./Widgets.css";
+import { TwitterTimelineEmbed, TwitterTweetEmbed } from "react-twitter-embed";
+import SearchIcon from "@mui/icons-material/Search";
 
 function Widgets() {
-    return <div>Widgets</div>;
+    return (
+        <div className="widgets">
+            <div className="widget-header">
+                <div className="widget-search-input">
+                    <input
+                        type="text"
+                        className="search-bar"
+                        placeholder="Search here"
+                    />
+                    <SearchIcon className="search-icon" />
+                </div>
+                <h2>What's happening?</h2>
+            </div>
+            <div className="widgets-list">
+                <TwitterTweetEmbed tweetId={"933354946111705097"} />
+                <TwitterTimelineEmbed
+                    sourceType="profile"
+                    screenName="elonmusk"
+                    options={{ height: 400 }}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default Widgets;
--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -18,14 +18,14 @@ function Home() {
         <div className="page-container">
             <div className="home-container">
                 <Grid container spacing={2}>
-                    <Grid item xs={3} sm={4} md={3} lg={3} xl={3}>
+                    <Grid item xs={3} sm={3} md={3} lg={3} xl={3}>
                         <Sidebar handleLogout={handleLogout} user={user} />
                     </Grid>
                     <Grid item xs={6} sm={5} md={6} lg={6} xl={6}>
                         {/* this Outlet component is responsible for rendering the Bookmarks,Messages,Profile components within the Parent (Home) Layout through the nested route in App.js */}
                         <Outlet />
                     </Grid>
-                    <Grid item xs={3} sm={3} md={3} lg={3} xl={3}>
+                    <Grid item xs={3} sm={4} md={3} lg={3} xl={3}>
                         <Widgets />
                     </Grid>
                 </Grid>
